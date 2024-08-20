2e68db28d4024942832597e727736e6ca970da84
KennethC12 <kchen122804@gmail.com>
2024-08-19 23:28:41 -0400

    first commit

---
+++ src/components/About/about.jsx
@@ -0,0 +1,22 @@
+import "./about.scss"
+
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="imagecontainer">
+        <img src="/Personal-Website/images/me.jpg" alt="" />
+        
+      </div>
+      <div className="textcontainer">
+        <h1>About Me</h1>
+        <p>Hi, my name is Kenneth Chen. I'm currently a Sophomore at Umass Boston studying computer science. 
+          Some of my hobbies are playing video games, volleyball, and basketball. I love learning new languages and much more.</p>
+      </div>
+
+    </div>
+  )
+}
+
+
+export default About
